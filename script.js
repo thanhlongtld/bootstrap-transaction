3ab06ea19d240a63dd8f6559861697e91b6b6fc1
thanhlongtld <thanhlongtld@gmail.com>
2021-10-21 23:26:26 +0700

    first push

---
+++ script.js
@@ -0,0 +1,117 @@
+$(document).ready(function () {
+  $('.table-wrapper').hide();
+  const allTransTable = $('#allTransactionsTable');
+  $('#allWrapper').show();
+  const waitingConfirmTable = $('#waitingConfirmTable');
+  const waitingShipTable = $('#waitingShipTable');
+  const shippingTable = $('#shippingTable');
+  const successTable = $('#successTable');
+  const failedTable = $('#failedTable');
+  const importTable = $('#importTable');
+  allTransTable.DataTable({
+    paging: true,
+    searching: false,
+    bLengthChange: false,
+    ordering: false,
+    language: {
+      info: 'Hiển thị từ _START_ đến _END_ của _TOTAL_ đơn hàng',
+      paginate: {
+        previous: '<',
+        next: '>',
+      },
+    },
+    pagingType: 'simple',
+  });
+  waitingConfirmTable.DataTable({
+    paging: true,
+    searching: false,
+    bLengthChange: false,
+    ordering: false,
+    language: {
+      info: 'Hiển thị từ _START_ đến _END_ của _TOTAL_ đơn hàng',
+      paginate: {
+        previous: '<',
+        next: '>',
+      },
+    },
+    pagingType: 'simple',
+  });
+  waitingShipTable.DataTable({
+    paging: true,
+    searching: false,
+    bLengthChange: false,
+    ordering: false,
+    language: {
+      info: 'Hiển thị từ _START_ đến _END_ của _TOTAL_ đơn hàng',
+      paginate: {
+        previous: '<',
+        next: '>',
+      },
+    },
+    pagingType: 'simple',
+  });
+  shippingTable.DataTable({
+    paging: true,
+    searching: false,
+    bLengthChange: false,
+    ordering: false,
+    language: {
+      info: 'Hiển thị từ _START_ đến _END_ của _TOTAL_ đơn hàng',
+      paginate: {
+        previous: '<',
+        next: '>',
+      },
+    },
+    pagingType: 'simple',
+  });
+  successTable.DataTable({
+    paging: true,
+    searching: false,
+    bLengthChange: false,
+    ordering: false,
+    language: {
+      info: 'Hiển thị từ _START_ đến _END_ của _TOTAL_ đơn hàng',
+      paginate: {
+        previous: '<',
+        next: '>',
+      },
+    },
+    pagingType: 'simple',
+  });
+  failedTable.DataTable({
+    paging: true,
+    searching: false,
+    bLengthChange: false,
+    ordering: false,
+    language: {
+      info: 'Hiển thị từ _START_ đến _END_ của _TOTAL_ đơn hàng',
+      paginate: {
+        previous: '<',
+        next: '>',
+      },
+    },
+    pagingType: 'simple',
+  });
+  importTable.DataTable({
+    paging: true,
+    searching: false,
+    bLengthChange: false,
+    ordering: false,
+    language: {
+      info: 'Hiển thị từ _START_ đến _END_ của _TOTAL_ đơn hàng',
+      paginate: {
+        previous: '<',
+        next: '>',
+      },
+    },
+    pagingType: 'simple',
+  });
+});
+
+const changeTab = (e, name) => {
+  e.preventDefault();
+  $('.nav-link').removeClass('active');
+  $(e.target).addClass('active');
+  $('.table-wrapper').hide();
+  $(`#${name}Wrapper`).show();
+};
